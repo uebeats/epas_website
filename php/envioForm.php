7986038ef3e2cfc928b41c41a4dbaed843e4b7d2
Jesús Caballero Peralta <uebeats@gmail.com>
2020-01-16 10:34:55 -0300

    Update envioMail.php

--- php/envioForm.php
+++ php/envioForm.php
@@ -1,64 +1,35 @@
 <?php
-    /*-------------------------
-    Autor: Jesus Caballero
-    Web: propiedadesdng.com
-    Mail: jesus@propiedadesdng.com
-    ---------------------------*/
+if (isset($_POST['email'])) {
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-    use PHPMailer\PHPMailer\SMTP;
+	// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+	$email_to = "diseno3@igospel.cl";
+	$email_subject = "Contacto desde el sitio web";
 
-    require 'PHPMailer/src/Exception.php';
-    require 'PHPMailer/src/PHPMailer.php';
-    require 'PHPMailer/src/SMTP.php';
+	// Aquí se deberían validar los datos ingresados por el usuario
+	if (
+		!isset($_POST['name']) ||
+		!isset($_POST['email']) ||
+		!isset($_POST['phone']) ||
+		!isset($_POST['mensaje'])
+	) {
 
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+		echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+		echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+		die();
+	}
 
-    try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'mail.epas.cl';                    	// Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'soporte@epas.cl';        // SMTP username
-        $mail->Password   = 'igospel2019';                             // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->CharSet    = 'UTF-8';                                // Activa la condificacción utf-8
-        $mail->Port       = 587;                                    // TCP port to connect to
+	$email_message = "Detalles del formulario de contacto:\n\n";
+	$email_message .= "Nombre: " . $_POST['name'] . "\n";
+	$email_message .= "E-mail: " . $_POST['email'] . "\n";
+	$email_message .= "Teléfono: " . $_POST['fono'] . "\n";
+	$email_message .= "Comentarios: " . $_POST['mensaje'] . "\n\n";
 
-        // $emailTo = 'uebeats@gmail.com';
-        // $nameTo = 'Jesus Caballero';
-        // $phoneTo = '948992070';
-        // $mensajeTo = 'Texto de prueba para mensaje';
 
-        $emailTo = $_POST['email'];
-        $nameTo = $_POST['name'];
-        $phoneTo = $_POST['fono'];
-        $mensajeTo = $_POST['mensaje'];
+	// Ahora se envía el e-mail usando la función mail() de PHP
+	$headers = 'From: ' . $email_from . "\r\n" .
+		'Reply-To: ' . $email_from . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+	@mail($email_to, $email_subject, $email_message, $headers);
 
-        //Recipients
-        $mail->setFrom('plataforma@epas.cl', 'EPAS LIMITADA | CONTACTO');
-        $mail->addAddress($emailTo, $nameTo);                       // Add a recipient
-        $mail->addReplyTo('plataforma@epas.cl', 'EPAS LIMITADA | CONTACTO');
-
-        $mail->addCC('soporte@epas.cl');
-        // $mail->addBCC('bcc@example.com');
-
-        // Attachments
-        // $mail->addAttachment('sendMail.php');            // Add attachments
-        // $mail->addAttachment($filename, 'Nota de Pago');            // Optional name
-
-        // Content
-        $mail->isHTML(true);                                        // Set email format to HTML
-        $mail->Subject = 'Formulario de Contacto | EPAS LIMITADA';
-        $mail->Body = '<b>Formulario de Contacto</b><br><br> Has recibido un formulario de contacto del sitio web EPAS LIMITADA.<br><br><b>Nombre:</b> '.$nameTo.'<br><b>Teléfono:</b> '.$phoneTo.'<br><b>Correo:</b> '.$emailTo.'<br><b>Mensaje:</b> '.$mensajeTo.'<br><br><small>Formulario ha sido enviado desde el sitio web (https://epas.cl)</small>';
-        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        $mail->send();
-
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
-?>
+	echo "¡El formulario se ha enviado con éxito!";
+}
